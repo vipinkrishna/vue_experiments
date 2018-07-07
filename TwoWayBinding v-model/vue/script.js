dfc173abc1c65b4adf759b5747f9398e24500f24
vipinkrishna <vipinkrishna.in@gmail.com>
2018-07-07 13:18:59 +0530

    Two Way Binding v-model

---
+++ TwoWayBinding v-model/vue/script.js
@@ -0,0 +1,12 @@
+// console.log(Vue)
+
+new Vue({
+    el: "#parent",
+    data: {
+        txtInputVariable: "Hello!",
+        selectInputVariable: "Something"
+    },
+    methods: {
+       
+    }
+});
