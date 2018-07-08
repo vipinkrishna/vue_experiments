adbcdd9b4ad23fb21fd9bd0e0af82561aa3eeceb
vipinkrishna <vipinkrishna.in@gmail.com>
2018-07-08 14:18:04 +0530

    computed dependency

--- ComputedDependency/vue/script.js
+++ ComputedDependency/vue/script.js
@@ -1,21 +1,29 @@
 // console.log(Vue)
 
-new Vue({
+const vm = new Vue({
     el: "#parent",
     data: {
         name: "computed",
-        anothername: "methods"
+        anothername: "methods",
+        yetanothername: "methodsstar"
     },
     computed: {
         computed_prop() {
             console.log("computed_prop called");
-            return this.name + ".com";
+            return this.name + ".com";  //UNCOMMENT AND SEE THE DIFFERENCE
+            // return "java" + ".com";  //UNCOMMENT AND SEE THE DIFFERENCE
         }
     },
     methods: {
-       methods_prop(){
-           console.log("methods_prop() called");
-           return this.anothername + ".com";
-       }
+        methods_prop() {
+            console.log("methods_prop() called");
+            return this.anothername + ".com";
+        },
+        methods_starprop() {
+            console.log("methodsstar_prop() called");
+            return this.yetanothername + ".com";
+        }
     }
 });
+
+// setInterval(()=>{vm.anothername ="UPDATE TEST!"},5000);
