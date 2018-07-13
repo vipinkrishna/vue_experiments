e2b928b09b66f26cd8ddf1ee97b82a483e585532
vipinkrishna <vipinkrishna.in@gmail.com>
2018-07-13 10:08:50 +0530

    Component Example

---
+++ Components/vue/script.js
@@ -0,0 +1,16 @@
+// console.log(Vue)
+
+Vue.component("hello-view",{
+    template: "<div class='circle'></div>"
+});
+
+const vm = new Vue({
+    el: "#parent",
+    data: {
+    },
+    computed: {
+    },
+    methods: {
+    }
+});
+
